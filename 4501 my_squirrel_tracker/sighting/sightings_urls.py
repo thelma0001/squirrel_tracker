9f5cf0ac518cb549258dd57d5d47c7bf88c75df6
unknown <angela.qi.shang@gmail.com>
2020-10-21 10:39:05 +0800

    add views and import/export

--- 4501 my_squirrel_tracker/sighting/sightings_urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-urlpatterns = [
-    url(r'^sightings/$', views.sightings),
-    url(r'^sightings/add$', views.add),
-    url(r'^sightings/update$', views.update),
-    url(r'^sightings/stats$', views.stats),
-    url(r'', views.sightings),
-]
